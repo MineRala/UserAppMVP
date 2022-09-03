3439ddb763d4d1ef85e49e38063b4e8e5c4382a0
Mine Rala <mine.rala@rapsodo.com>
2022-09-03 21:50:52 +0300

    Initial Commit

---
+++ UserAppMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserAppMVP
+//
+//  Created by Mine Rala on 3.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
